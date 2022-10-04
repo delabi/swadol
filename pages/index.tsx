999f61fae0039650f821b877d9ad5bc6e2c0abb2
Laban Bwire <bwire.laban@gmail.com>
2022-10-04 20:07:34 +0300

    second push

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,36 @@
 import { observer, useLocalObservable } from 'mobx-react-lite'
+import { useEffect } from 'react'
 import Guess from '../components/Guess'
 import Qwerty from '../components/Qwerty'
 import PuzzleStore from '../stores/PuzzleStore'
 
 export default observer(function Home() {
   const store = useLocalObservable(() => PuzzleStore)
+  useEffect(() => {
+    store.init()
+    window.addEventListener('keyup', store.handleKeyup)
+
+    return () => {
+      window.removeEventListener('keyup', store.handleKeyup)
+    }
+  }, [])
   return (
-  <div className="flex h-screen w-scren flex-col items-center justify-center bg-gray-600">
-    <h1 className="text-6xl font-bold uppercase text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-green-400">Wordle</h1>
-    {new Array(6).fill(0).map(_,i) => (
-      <Guess word={'tests'} guess={'guess'} isGuessed={false}/>
-    )}
-    
-    <h1>won/loss</h1>
-    <Qwerty />
-  </div>
+    <div className="flex h-screen w-scren flex-col items-center justify-center bg-gray-600">
+      <h1 className="text-6xl font-bold uppercase text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-green-400">Wordle
+      </h1>
+      {store.guesses.map((_, i) => (
+        <Guess 
+        key={i}
+        word={store.word} 
+        guess={store.guesses[i]} 
+        isGuessed={i < store.currentGuess}/>
+      ))}
+      {store.won && <h1>You won!</h1>}
+      {store.lost && <h1>You lost!</h1>}
+      {(store.won || store.lost) && (<button onClick={store.init}>Play Again</button>)}
+      <Qwerty store={store}/>
+      word: {store.word} <br/>
+      guesses: {JSON.stringify(store.guesses)}
+    </div>
   )
 })
